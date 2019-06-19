1090525115c32af1a6a7c42fd2932be458d5206d
Anderson Souza <jair_anderson_bs@hotmail.com>
2019-06-18 23:15:05 -0300

    Comentando security

--- src/main/java/io/github/jass2125/beer/api/infraestrutura/seguranca/usuario/MyAuthUser.java
+++ src/main/java/io/github/jass2125/beer/api/infraestrutura/seguranca/usuario/MyAuthUser.java
@@ -1,74 +1,69 @@
-package io.github.jass2125.beer.api.infraestrutura.seguranca.usuario;
-
-import java.util.Arrays;
-import java.util.Objects;
-import org.springframework.security.core.userdetails.User;
-
-
-
-/**
- *
- * @author victor-hugo
- */
-public class MyAuthUser extends User {
-
-    private Long id;
-    private String email;
-    private String nome;
-
-    public MyAuthUser(String nome, String perfil) {
-        super(nome, "", Arrays.asList(null));
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 79 * hash + Objects.hashCode(this.id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final MyAuthUser other = (MyAuthUser) obj;
-        return Objects.equals(this.id, other.id);
-    }
-
-    @Override
-    public String toString() {
-        return "MyAuthUser{" + "id=" + id + ", email=" + email + ", nome=" + nome + '}';
-    }
-
-}
+//package io.github.jass2125.beer.api.infraestrutura.seguranca.usuario;
+//
+//import java.util.Arrays;
+//import java.util.Objects;
+//import org.springframework.security.core.userdetails.User;
+//
+//
+//public class MyAuthUser extends User {
+//
+//    private Long id;
+//    private String email;
+//    private String nome;
+//
+//    public MyAuthUser(String nome, String perfil) {
+//        super(nome, "", Arrays.asList(null));
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 79 * hash + Objects.hashCode(this.id);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final MyAuthUser other = (MyAuthUser) obj;
+//        return Objects.equals(this.id, other.id);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MyAuthUser{" + "id=" + id + ", email=" + email + ", nome=" + nome + '}';
+//    }
+//
+//}
--- src/main/java/io/github/jass2125/beer/api/infraestrutura/seguranca/usuario/MyUserDetails.java
+++ src/main/java/io/github/jass2125/beer/api/infraestrutura/seguranca/usuario/MyUserDetails.java
@@ -1,38 +1,38 @@
-package io.github.jass2125.beer.api.infraestrutura.seguranca.usuario;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import io.github.jass2125.beer.api.usuario.Usuario;
-import io.github.jass2125.beer.api.usuario.servico.UsuarioService;
-
-@Service
-public class MyUserDetails implements UserDetailsService {
-
-    @Autowired
-    private UsuarioService usuarioService;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        final Usuario usuario = usuarioService.getByEmail(email);
-
-        if (usuario == null) {
-            throw new UsernameNotFoundException("User '" + email + "' not found");
-        }
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(usuario.getPerfil()));
-
-        final MyAuthUser myAuthUser = new MyAuthUser(usuario.getNome(), usuario.getPerfil());
-
-        return myAuthUser;
-    }
-
-}
+//package io.github.jass2125.beer.api.infraestrutura.seguranca.usuario;
+//
+//import io.github.jass2125.beer.api.usuario.Usuario;
+//import io.github.jass2125.beer.api.usuario.servico.UsuarioService;
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class MyUserDetails implements UserDetailsService {
+//
+//    @Autowired
+//    private UsuarioService usuarioService;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        final Usuario usuario = usuarioService.getByEmail(email);
+//
+//        if (usuario == null) {
+//            throw new UsernameNotFoundException("User '" + email + "' not found");
+//        }
+//
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        authorities.add(new SimpleGrantedAuthority(usuario.getPerfil()));
+//
+//        final MyAuthUser myAuthUser = new MyAuthUser(usuario.getNome(), usuario.getPerfil());
+//
+//        return myAuthUser;
+//    }
+//
+//}
